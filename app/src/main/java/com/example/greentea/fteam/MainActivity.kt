719379438414cf60275595e448e81ff09c3622a3
mnm234 <minatatsu0373.hornpiano@gmail.com>
2018-10-31 14:26:56 +0900

    とりあえず空のフラグメント作りました

--- app/src/main/java/com/example/greentea/fteam/MainActivity.kt
+++ app/src/main/java/com/example/greentea/fteam/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.greentea.fteam
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,11 +12,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-//        val transaction = supportFragmentManager.beginTransaction()
-//
-//        val homeFragment = HomeFragment()
-//        transaction.replace(R.id.container, homeFragment)
-//        transaction.commit()
+        val homeFragment = HomeFragment()
+        val playlistFragment = PlaylistFragment()
+        val rankingFragment = RankingFragment()
+        val historyFragment = HistoryFragment()
+        val uploadFragment = UploadFragment()
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, homeFragment)
+        transaction.commit()
+
+
+        navigation_bottom.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, homeFragment)
+                    transaction.commit()
+
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_ranking -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, rankingFragment)
+                    transaction.commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_playlist -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, playlistFragment)
+                    transaction.commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_history -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, historyFragment)
+                    transaction.commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_upload -> {
+                    val transaction = supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, uploadFragment)
+                    transaction.commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
 
     }
 }
