da570c5d4d9cdde4dd38dee24cb80259e92550f6
MAETEL\2160010 <i_k_d42_xxx@yahoo.co.jp>
2018-11-01 14:45:49 +0900

    NewCompeFragment

--- app/src/main/java/com/example/greentea/fteam/NewCompeFragment.kt
+++ app/src/main/java/com/example/greentea/fteam/NewCompeFragment.kt
@@ -16,7 +16,6 @@ class NewCompeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_compe, container, false)
     }
 
