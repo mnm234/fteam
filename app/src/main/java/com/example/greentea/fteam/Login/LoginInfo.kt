1d196a1840cdeef3bdbaf77adc10c37a3a09e72b
mnm234 <minatatsu0373.hornpiano@gmail.com>
2019-01-20 13:23:20 +0900

    LoginActivity 54行目 SignInエラー

---
+++ app/src/main/java/com/example/greentea/fteam/Login/LoginInfo.kt
@@ -0,0 +1,2 @@
+package com.example.greentea.fteam.Login
+
---
+++ app/src/main/java/com/example/greentea/fteam/Login/PleaseLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.greentea.fteam.Login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PleaseLoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_please_login)
+    }
+}
