9304834c0410086550188ab7d1c2fcaf493b8414
A-yoneda <abesi5991@gmail.com>
2019-01-21 02:58:17 +0900

    newFragmentにCompListを仮設置 そこからCompの詳細画面に飛んでチャレンジボタンで既存の競技に投稿できるようにした

--- app/src/main/java/com/example/greentea/fteam/MainActivity.kt
+++ app/src/main/java/com/example/greentea/fteam/MainActivity.kt
@@ -1,18 +1,13 @@
 package com.example.greentea.fteam
 
-import android.content.ClipData
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import com.example.greentea.fteam.contribution.UploadFragment
-import com.example.greentea.fteam.contribution.record.VideoFragment
 import com.example.greentea.fteam.home.CompDetailFragment
 import com.example.greentea.fteam.home.HomeFragment
 import com.example.greentea.fteam.newComp.NewFragment
@@ -30,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 //    internal var search_menu: Menu? = null
 //    internal var item_search: MenuItem? = null
 
-    private var navi:View? = null
+//    private var navi:View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
         val bottomNaviId = intent.getIntExtra(MAIN_BOTTOM_NAV_KEY, 0)
-        navi = findViewById<View>(R.id.navi_menu)
+//        navi = findViewById<View>(R.id.navi_menu)
 
         when (bottomNaviId) {
             MAIN_HOME_BOTTOM_NAV_ID -> {
