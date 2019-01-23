a64f363544e2305e0aa2cc4e97281271190555e5
mnm234 <minatatsu0373.hornpiano@gmail.com>
2019-01-24 01:26:33 +0900

    MyPage

--- app/src/main/java/com/example/greentea/fteam/object/UserObject.kt
+++ app/src/main/java/com/example/greentea/fteam/object/UserObject.kt
@@ -1,5 +1,7 @@
 package com.example.greentea.fteam.`object`
 
+import android.graphics.Bitmap
+
 data class UserObject(
         val name: String = "",
         val followerID: ArrayList<String> = ArrayList(),
